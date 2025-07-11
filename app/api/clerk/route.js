828949c56b52aa63bbbef013dea412cd551fd86f
AppaniSandeep <appanisandeep18@gmail.com>
2025-07-11 17:06:45 +0530

    Updated user.js and env files

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -40,7 +40,7 @@ export async function POST(req){
             await User.create(userData)
             break;
         case "user.updated":
-            await User.findByIdAndUpdate(c, userData)
+            await User.findByIdAndUpdate(data.id, userData)
             break;
         case "user.deleted":
             await User.findByIdAndDelete(userData)
